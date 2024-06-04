cb52833cabf876ff2366e4108e254f172b8926ba
azizkhonteacher <azizkhonteacher@gmail.com>
2024-06-04 17:21:07 +0500

    update header and footer

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,79 @@
 <template>
-  <header>
-    <div class="container">
-      <div class="site__logo">
-        <img src="./assets/img/svg/myteam-logo.svg" alt="img" />
-      </div>
-      <div class="site__nav">
-        <ul>
-          <li>
-            <a href="#">home</a>
-          </li>
-          <li>
-            <a href="#">about</a>
-          </li>
-        </ul>
-      </div>
-      <div class="site__btn">
-        <button>contact us</button>
-      </div>
-    </div>
-  </header>
-
-  <footer>
-    <div class="container">
-      <div class="site__footer-nav">
+  <div>
+    <header>
+      <div class="container">
         <div class="site__logo">
           <img src="./assets/img/svg/myteam-logo.svg" alt="img" />
         </div>
-
         <div class="site__nav">
-        <ul>
-          <li>
-            <a href="#">home</a>
-          </li>
-          <li>
-            <a href="#">about</a>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <a href="#">home</a>
+            </li>
+            <li>
+              <a href="#">about</a>
+            </li>
+          </ul>
+        </div>
+        <div class="site__btn">
+          <button>contact us</button>
+        </div>
       </div>
-      </div>
+    </header>
+    
+    <footer>
+      <div class="container">
+        <div class="site__footer-nav">
+          <div class="site__logo">
+            <img src="./assets/img/svg/myteam-logo.svg" alt="img" />
+          </div>
 
-      <div class="site__footer-paragraph">
-        <p>987  Hillcrest Lane</p>
-        <p>Irvine, CA</p>
-        <p>California 92714</p>
-        <p>Call Us : 949-833-7432</p>
-      </div>
-
-      <div class="site__footer-icon">
-        <div class="icons">
-          <a href="#"><img src="./assets/img/svg/icon-facebook.svg" alt="img" class="footer-icon"></a>
-          <a href="#"><img src="./assets/img/svg/icon-pinterest.svg" alt="img" class="footer-icon"></a>
-          <a href="#"><img src="./assets/img/svg/icon-twitter.svg" alt="img" class="footer-icon"></a>
+          <div class="site__nav">
+            <ul>
+              <li>
+                <a href="#">home</a>
+              </li>
+              <li>
+                <a href="#">about</a>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <p class="footer__parag">Copyright 2020. All Rights Reserved</p>
+        <div class="site__footer-paragraph">
+          <p>987 Hillcrest Lane</p>
+          <p>Irvine, CA</p>
+          <p>California 92714</p>
+          <p>Call Us : 949-833-7432</p>
+        </div>
+
+        <div class="site__footer-icon">
+          <div class="icons">
+            <a href="#"
+              ><img
+                src="./assets/img/svg/icon-facebook.svg"
+                alt="img"
+                class="footer-icon"
+            /></a>
+            <a href="#"
+              ><img
+                src="./assets/img/svg/icon-pinterest.svg"
+                alt="img"
+                class="footer-icon"
+            /></a>
+            <a href="#"
+              ><img
+                src="./assets/img/svg/icon-twitter.svg"
+                alt="img"
+                class="footer-icon"
+            /></a>
+          </div>
+
+          <p class="footer__parag">Copyright 2020. All Rights Reserved</p>
+        </div>
       </div>
-    </div>
-  </footer>
+    </footer>
+  </div>
 </template>
 
 <script>
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="home__main">
+            <div class="home__head">
+                <h2 class="home__head-title">
+                    Find the best <span>talent</span>
+                </h2>
+
+                <div class="home__head-info">
+                    <span class="home__head-info-span"></span>
+                    <p class="home__head-info-p">
+                        Finding the right people and building high performing teams can be hard. Most companies aren’t tapping into the abundance of global talent. We’re about to change that.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+
+
+<script setup>
+   
+</script>
+
+<style lang="scss" scoped>
+
+</style>
