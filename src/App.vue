de032f666c397a4e5e98a9d138fc815d318060d0
azizkhonteacher <azizkhonteacher@gmail.com>
2024-06-04 17:02:08 +0500

    header-vs-footer

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <div>
-    <div class="header">hello </div>
-  </div>
+  <header>
+    <div class="container">
+      <div class="site__logo">
+        <img src="./assets/img/svg/myteam-logo.svg" alt="img" />
+      </div>
+      <div class="site__nav">
+        <ul>
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">about</a>
+          </li>
+        </ul>
+      </div>
+      <div class="site__btn">
+        <button>contact us</button>
+      </div>
+    </div>
+  </header>
+
+  <footer>
+    <div class="container">
+      <div class="site__footer-nav">
+        <div class="site__logo">
+          <img src="./assets/img/svg/myteam-logo.svg" alt="img" />
+        </div>
+
+        <div class="site__nav">
+        <ul>
+          <li>
+            <a href="#">home</a>
+          </li>
+          <li>
+            <a href="#">about</a>
+          </li>
+        </ul>
+      </div>
+      </div>
+
+      <div class="site__footer-paragraph">
+        <p>987  Hillcrest Lane</p>
+        <p>Irvine, CA</p>
+        <p>California 92714</p>
+        <p>Call Us : 949-833-7432</p>
+      </div>
+
+      <div class="site__footer-icon">
+        <div class="icons">
+          <a href="#"><img src="./assets/img/svg/icon-facebook.svg" alt="img" class="footer-icon"></a>
+          <a href="#"><img src="./assets/img/svg/icon-pinterest.svg" alt="img" class="footer-icon"></a>
+          <a href="#"><img src="./assets/img/svg/icon-twitter.svg" alt="img" class="footer-icon"></a>
+        </div>
+
+        <p class="footer__parag">Copyright 2020. All Rights Reserved</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
